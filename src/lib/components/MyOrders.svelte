ee366433aaca6fb9fd6cd2c817588b387f385b1f
Daniel <carlosbrizuelamereles@gmail.com>
2023-06-14 20:22:13 -0400

    The my orders component has been completed

---
+++ src/lib/components/MyOrders.svelte
@@ -0,0 +1,43 @@
+<!-- cuerpo -->
+<div class="w-full h-screen grid place-items-center">
+	<!-- contenedor de la tienda -->
+	<div class="w-[300px]">
+		<h1 class="text-bold mb-10">Mis ordenes</h1>
+
+		<!-- contenedor de mis pedidos -->
+		<div class="flex flex-col">
+			<div class="grid grid-template-columns-auto gap-4 items-center mb-3">
+				<p class="flex flex-col">
+					<span class="text-regular font-bold">03.25.21</span>
+					<span class="text-compact text-global-veryLigthPink">2 articles</span>
+				</p>
+				<p class="text-end font-bold">$820.00</p>
+				<img src="./icons/flechita.svg" alt="arrow" />
+				<p class="flex flex-col">
+					<span class="text-regular font-bold">03.25.21</span>
+					<span class="text-compact text-global-veryLigthPink">6 articles</span>
+				</p>
+				<p class="text-end font-bold">$450.00</p>
+				<img src="./icons/flechita.svg" alt="arrow" />
+				<p class="flex flex-col">
+					<span class="text-regular font-bold">03.25.21</span>
+					<span class="text-compact text-global-veryLigthPink">8 articles</span>
+				</p>
+				<p class="text-end font-bold">$120.00</p>
+				<img src="./icons/flechita.svg" alt="arrow" />
+				<p class="flex flex-col">
+					<span class="text-regular font-bold">03.25.21</span>
+					<span class="text-compact text-global-veryLigthPink">2 articles</span>
+				</p>
+				<p class="text-end font-bold">$200.00</p>
+				<img src="./icons/flechita.svg" alt="arrow" />
+				<p class="flex flex-col">
+					<span class="text-regular font-bold">03.25.21</span>
+					<span class="text-compact text-global-veryLigthPink">7 articles</span>
+				</p>
+				<p class="text-end font-bold">$302.00</p>
+				<img src="./icons/flechita.svg" alt="arrow" />
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/order/+page.svelte
+++ src/routes/order/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import MyOrder from '$lib/components/MyOrder.svelte';
+	import MyOrders from '$lib/components/MyOrders.svelte';
 </script>
 
 <main class="m-0">
-	<MyOrder />
+	<!-- <MyOrder /> -->
+	<MyOrders />
 </main>
